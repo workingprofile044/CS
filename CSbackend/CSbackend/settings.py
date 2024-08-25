11128578dde9a48bb95451fe5f2f46380664e47d
Sengoku <149186370+TenjinSengoku@users.noreply.github.com>
2024-08-26 05:34:24 +0800

    commit 004

--- CSbackend/CSbackend/settings.py
+++ CSbackend/CSbackend/settings.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 from datetime import timedelta
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
