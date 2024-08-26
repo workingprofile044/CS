11412864fd191c5d889538a0ba4f7f0c8d9cb782
Sengoku <149186370+TenjinSengoku@users.noreply.github.com>
2024-08-26 09:15:37 +0800

    commit 023

--- CSbackend/storage/urls.py
+++ CSbackend/storage/urls.py
@@ -1,10 +1,7 @@
-# storage/urls.py
-
-from django.urls import path, include
+from django.urls import path
 from .views import FileListView, FileUploadView, FileDeleteView, FileRenameView
 
 urlpatterns = [
-    path('api/users/', include('storage.urls')),
     path('files/', FileListView.as_view(), name='file-list'),
     path('upload/', FileUploadView.as_view(), name='file-upload'),
     path('delete/<int:pk>/', FileDeleteView.as_view(), name='file-delete'),
--- CSbackend/users/urls.py
+++ CSbackend/users/urls.py
@@ -1,10 +1,7 @@
-# users/urls.py
-
-from django.urls import path, include
+from django.urls import path
 from .views import RegisterView, LoginView, LogoutView, AdminUserListView
 
 urlpatterns = [
-    path('api/users/', include('users.urls')),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
