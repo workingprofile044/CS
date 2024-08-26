5d5d80527544bc867a91b9a8fbfc448cdceb7aef
Sengoku <149186370+TenjinSengoku@users.noreply.github.com>
2024-08-26 13:21:51 +0800

    commit 042

--- csfrontend/src/components/Logout.js
+++ csfrontend/src/components/Logout.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axiosInstance from '../axiosConfig';
+
 
 function Logout({ onLogout }) {
     const navigate = useNavigate();
