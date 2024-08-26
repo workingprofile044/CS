f09fa8a571b6610b7d28c1fb47e4299723f8a43a
Sengoku <149186370+TenjinSengoku@users.noreply.github.com>
2024-08-26 12:52:59 +0800

    commit 039

--- csfrontend/src/App.js
+++ csfrontend/src/App.js
@@ -1,11 +1,3 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './components/Home';
-import Register from './components/Register';
-import Login from './components/Login';
-import FileUpload from './components/FileUpload';
-import FileList from './components/FileList';
-import Navigation from './components/Navigation';
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Navigation from './components/Navigation';
