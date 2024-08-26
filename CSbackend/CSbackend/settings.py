f613ed331e8780a4442734d7ad73d9e9f61e8482
Sengoku <149186370+TenjinSengoku@users.noreply.github.com>
2024-08-26 08:06:22 +0800

    commit 021

--- CSbackend/CSbackend/settings.py
+++ CSbackend/CSbackend/settings.py
@@ -31,12 +31,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'users',
     'storage',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
