8baaee1a1999ffb3427005c93103c4f9230b383e
Sengoku <149186370+TenjinSengoku@users.noreply.github.com>
2024-08-27 07:49:23 +0800

    commit 061

--- csfrontend/src/axiosConfig.js
+++ csfrontend/src/axiosConfig.js
@@ -3,9 +3,6 @@ import axios from 'axios';
 const axiosInstance = axios.create({
     baseURL: 'http://89.111.175.161:8000',
     timeout: 10000,
-    headers: {
-        'Content-Type': 'application/json',
-    }
 });
 
 axiosInstance.interceptors.request.use(
