e5ad604898be8f1cde5961e6e2bc6f396e27ca2c
Sengoku <149186370+TenjinSengoku@users.noreply.github.com>
2024-08-28 20:28:17 +0800

    Finish?

--- csfrontend/src/components/Home.js
+++ csfrontend/src/components/Home.js
@@ -2,17 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
 
-
 function Home() {
     return (
         <div className="home-container">
             <h1>Добро пожаловать в приложение CS</h1>
-            <p>Это веб-приложение позволяет вам управлять своими файлами безопасно и эффективно.</p>
+            <p>Это веб-приложение позволяет вам управлять своими файлами.</p>
             <p>
                 Пожалуйста, <Link to="/register">Зарегистрируйтесь</Link> или <Link to="/login">Войдите</Link> для продолжения.
+            </p>
+            <p>
+                Исходный код проекта доступен на <a href="https://github.com/workingprofile044/CS">GitHub</a>.
             </p>
         </div>
     );
 }
 
 export default Home;
+
