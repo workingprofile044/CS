124057d8f01a650ecfcd2d4af2a4e5095d6876dc
Sengoku <149186370+TenjinSengoku@users.noreply.github.com>
2024-08-28 15:20:02 +0800

    start05

--- csfrontend/src/axiosConfig.js
+++ csfrontend/src/axiosConfig.js
@@ -3,9 +3,6 @@ import axios from 'axios';
 const axiosInstance = axios.create({
     baseURL: 'http://89.111.175.161:8000',
     timeout: 10000,
-    headers: {
-        'Content-Type': 'application/json',
-    }
 });
 
 axiosInstance.interceptors.request.use(
@@ -14,9 +11,41 @@ axiosInstance.interceptors.request.use(
         if (token) {
             config.headers['Authorization'] = `Bearer ${token}`;
         }
+
+        if (!(config.data instanceof FormData)) {
+            config.headers['Content-Type'] = 'application/json';
+        }
+
         return config;
     },
     error => {
+        return Promise.reject(error);
+    }
+);
+
+axiosInstance.interceptors.response.use(
+    response => response,
+    async error => {
+        const originalRequest = error.config;
+
+        if (error.response.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+            const refreshToken = localStorage.getItem('refresh_token');
+            if (refreshToken) {
+                try {
+                    const response = await axiosInstance.post('/api/token/refresh/', { refresh: refreshToken });
+                    const newAccessToken = response.data.access;
+                    localStorage.setItem('access_token', newAccessToken);
+                    axiosInstance.defaults.headers['Authorization'] = `Bearer ${newAccessToken}`;
+                    originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+                    return axiosInstance(originalRequest);
+                } catch (refreshError) {
+                    console.error('Token refresh failed:', refreshError);
+                    return Promise.reject(refreshError);
+                }
+            }
+        }
+
         return Promise.reject(error);
     }
 );
